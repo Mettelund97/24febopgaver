98375f47606b95c13a853b1318eda0f6a93e2405
Mettelund97 <lu.nd3@hotmail.com>
2025-02-24 09:54:21 +0100

    h

---
+++ src/test.js
@@ -0,0 +1,8 @@
+const { add } = require('./opgave1.js');
+    test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+    test('throws error for non-number inputs', () => {
+    expect(() => add('1', 2)).toThrow('Inputs must be numbers');
+});
